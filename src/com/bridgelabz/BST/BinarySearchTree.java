ef9feb13646184b4e2bc804f7371588530637fcc
surabhisanjan <surabhisanjan@gmail.com>
2022-12-06 17:03:59 +0530

    initializing BST

---
+++ src/com/bridgelabz/BST/BinarySearchTree.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.BST;
+//Ability to create a BST with hashTable
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("Initiating the Binary Search Tree!!!");
+
+    }
+}
