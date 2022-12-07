7daedd2d1ce9b7980a22d38bb3bf50818fe67c91
surabhisanjan <surabhisanjan@gmail.com>
2022-12-07 16:44:59 +0530

    Addition: Creating new BST

--- src/com/bridgelabz/BST/BinarySearchTree.java
+++ src/com/bridgelabz/BST/BinarySearchTree.java
@@ -1,8 +1,36 @@
 package com.bridgelabz.BST;
-//Ability to create a BST with hashTable
+//Ability to create a BST by
+//adding 56 and then
+//adding 30 & 70 - Use INode to create My Binary
 public class BinarySearchTree {
+    private TreeNode root;
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+    public void insert(int value){
+        root = insert(root, value);
+    }
+    public TreeNode insert( TreeNode root, int value){
+        if (root == null || root.data == value){
+            root = new TreeNode(value);
+            return root;
+        }
+        if (value < root.data){
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
     public static void main(String[] args) {
-        System.out.println("Initiating the Binary Search Tree!!!");
-
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(56);
+        tree.insert(30);
+        tree.insert(70);
     }
 }
