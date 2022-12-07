486f89d514d3d6c09b10a66128a20737272247fe
surabhisanjan <surabhisanjan@gmail.com>
2022-12-07 17:13:19 +0530

    Addition: Ability to inorder and getting the Size of BST

--- src/com/bridgelabz/BST/BinarySearchTree.java
+++ src/com/bridgelabz/BST/BinarySearchTree.java
@@ -2,6 +2,8 @@ package com.bridgelabz.BST;
 //Ability to create a BST by
 //adding 56 and then
 //adding 30 & 70 - Use INode to create My Binary
+//Check if all are added with using size method in Binary Tree
+
 public class BinarySearchTree {
     private TreeNode root;
     private class TreeNode{
@@ -27,10 +29,41 @@ public class BinarySearchTree {
         }
         return root;
     }
+
+    public void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    int size() {
+        return size(root);
+    }
+    int size(TreeNode root) {
+        if (root == null)
+            return 0;//if tree is empty then return zero
+        else
+            return (size(root.left) + 1 + size(root.right));}
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
-        tree.insert(56);
+        tree.insert(56); //root node
         tree.insert(30);
+        tree.insert(22);
+        tree.insert(40);
+        tree.insert(11);
+        tree.insert(3);
+        tree.insert(16);
         tree.insert(70);
+        tree.insert(60);
+        tree.insert(95);
+        tree.insert(65);
+        tree.insert(63);
+        tree.insert(67);
+        tree.inOrder(tree.root);
+        System.out.println();
+        System.out.println("Size of the given tree is: " + tree.size());
     }
 }
